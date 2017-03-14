b309e85be753e5d25c010e9d3e5252b3b1dce220
Michael Cabus <alotofmath@gmail.com>
2017-03-14 08:24:08 -0400

    Update designscript.js

--- script/designscript.js
+++ script/designscript.js
@@ -34,6 +34,8 @@ $('#results #holdingbranch_id span').attr('data-controls', 'expand_1');
 
 $('#results #holdingbranch_id span').attr('aria-expanded', 'false');
 
+$("#itype_id ul").addClass("js-to_expand");
+
 
 
 $("#holdingbranch_id ul").addClass("js-to_expand");
@@ -54,8 +56,6 @@ $('#results #itype_id h5').attr('data-controls', 'expand_1');
 $('#results #itype_id h5').attr('aria-expanded', 'false');
 
 
-
-$("#itype_id ul").addClass("js-to_expand");
 
 $("#itype_id ul").addClass("expandmore__to_expand");
 
@@ -319,115 +319,7 @@ $("#holdings").html('<a href="'+iabks_link+'"><img src="http://media.bywatersolu
 })(jQuery);
 
 
-/* JS for Koha CoverFlow Plugin
-   This JS was added automatically by installing the CoverFlow plugin
-   Please do not modify */$(document).ready(function(){$.getScript("/plugin/Koha/Plugin/Com/ByWaterSolutions/CoverFlow/bower_components/jquery-flipster/dist/jquery.flipster.min.js",function(data,textStatus,jqxhr){$("head").append("<link id='flipster-css' href='/plugin/Koha/Plugin/Com/ByWaterSolutions/CoverFlow/bower_components/jquery-flipster/dist/jquery.flipster.min.css' type='text/css' rel='stylesheet' />");$('#coverflow').load("/coverflow.pl?id=265",function(){$('.koha-coverflow img').on("load",function(){if(this.naturalHeight==1){$(this).attr("src","http://media.bywatersolutions.com/Model/NoImage.png");}});var opt={'items':'.item','minfactor':15,'distribution':1.5,'scalethreshold':0,'staticbelowthreshold':false,'titleclass':'itemTitle','selectedclass':'selectedItem','scrollactive':true,'step':{'limit':4,'width':10,'scale':true}};$('#coverflow').flipster({});});});});
-/* End of JS for Koha CoverFlow Plugin */
 
-/*accessibility plugin*/
-
-/*jQuery(document).ready(function($){*/
-
-   /*
-    * jQuery simple and accessible hide-show system (collapsible regions), using ARIA
-    * @version v1.5.0
-    * Website: https://a11y.nicolas-hoffmann.net/hide-show/
-    * License MIT: https://github.com/nico3333fr/jquery-accessible-hide-show-aria/blob/master/LICENSE
-    */
-   // loading expand paragraphs
-   // these are recommended settings by a11y experts. You may update to fulfill your needs, but be sure of what you’re doing.
-   /*var attr_control = 'data-controls',
-       attr_expanded = 'aria-expanded',
-       attr_labelledby = 'data-labelledby',
-       attr_hidden = 'data-hidden',
-       $expandmore = $('.js-expandmore'),
-       $body = $('body'),
-       delay = 1500,
-       hash = window.location.hash.replace( "#", "" );
-
-
-   if ( $expandmore.length ) { // if there are at least one :)
-      $expandmore.each( function(index_to_expand) {
-          var $this = $(this) ,
-              index_lisible = index_to_expand+1,
-              options = $this.data(),
-              $hideshow_prefix_classes = typeof options.hideshowPrefixClass !== 'undefined' ? options.hideshowPrefixClass + '-' : '',
-              $to_expand = $this.next(".js-to_expand"),
-              $expandmore_text = $this.html();
-
-          $this.html( '<button type="button" class="' + $hideshow_prefix_classes + 'expandmore__button js-expandmore-button">' + $expandmore_text + '</button>' );
-          $button = $this.children('.js-expandmore-button');
-
-          $to_expand.addClass( $hideshow_prefix_classes + 'expandmore__to_expand' ).stop().delay( delay ).queue( function() {
-            var $this = $(this);
-            if ($this.hasClass('js-first_load')) {
-               $this.removeClass('js-first_load');
-            }
-          });
-
-          $button.attr('id', 'label_expand_' + index_lisible);
-          $button.attr(attr_control, 'expand_' + index_lisible);
-          $button.attr(attr_expanded, 'false');
-
-          $to_expand.attr('id', 'expand_' + index_lisible);
-          $to_expand.attr(attr_hidden, 'true');
-          $to_expand.attr(attr_labelledby, 'label_expand_' + index_lisible);
-
-          // quick tip to open (if it has class is-opened or if hash is in expand)
-          if ($to_expand.hasClass('is-opened') || ( hash !== "" && $to_expand.find($( "#" + hash )).length ) ){
-             $button.addClass('is-opened').attr(attr_expanded, 'true');
-             $to_expand.removeClass('is-opened').removeAttr(attr_hidden);
-          }
-
-
-      });
-
-
-   }
-
-
-   $body.on( 'click', '.js-expandmore-button', function( event ) {
-      var $this = $(this),
-          $destination = $( '#' + $this.attr(attr_control) );
-
-      if ($this.attr(attr_expanded) === 'false') {
-          $this.addClass('is-opened').attr(attr_expanded, 'true');
-          $destination.removeAttr(attr_hidden);
-         }
-         else {
-              $this.removeClass('is-opened').attr(attr_expanded, 'false');
-              $destination.attr(attr_hidden, 'true');
-              }
-
-      event.preventDefault();
-
-   });
-
-   $body.on( 'click keydown', '.js-expandmore', function( event ) {
-      var $this = $(this),
-          $target = $(event.target),
-          $button_in = $this.find( '.js-expandmore-button' );
-
-      if ( !$target.is($button_in) && !$target.closest($button_in).length ) {
-
-          if ( event.type == 'click' ){
-              $button_in.trigger('click');
-              return false;
-              }
-          if ( event.type == 'keydown' && (event.keyCode==13 || event.keyCode==32 )  ){
-              $button_in.trigger('click');
-              return false;
-              }
-
-          }
-
-
-   });
-
-
-});
-
-0*/
 
 
 
