2c1785ccefc05ace134a84ba8307e904118a4219
Michael Cabus <alotofmath@gmail.com>
2017-04-05 11:43:23 -0400

    Update designscript.js

--- script/designscript.js
+++ script/designscript.js
@@ -219,17 +219,17 @@ $(author).hide();
 $(window).load( function() {
 $(".coverimages img").addClass("item-thumbnail");
 
+ var addTo = $("#addto").html();
+
+console.log(addTo);
+
+$( "#facetcontainer" ).append('<section class="save_title"><section class="heading"><h4><a class="" href="#">Save Titles</a></h4></section><h4 class="save_text">Save Selected Titles To</h4>' + addTo + '<section class="save_action"><span class="checkall"><a id="CheckAll" href="#">Select all</a></span><span class="clearall"><a id="CheckNone" href="#">Clear all</a></span></section><input class="btn btn-small" value="Save" type="submit"><span id="placehold"><input class="hold disabled" value="Place hold" type="submit"></span></section>');
  
  
 });
 
 $(document).ready( function() {
 
- var addTo = $("#addto").html();
-
-console.log(addTo);
-
-$( "#facetcontainer" ).append('<section class="save_title"><section class="heading"><h4><a class="" href="#">Save Titles</a></h4></section><h4 class="save_text">Save Selected Titles To</h4>' + addTo + '<section class="save_action"><span class="checkall"><a id="CheckAll" href="#">Select all</a></span><span class="clearall"><a id="CheckNone" href="#">Clear all</a></span></section><input class="btn btn-small" value="Save" type="submit"><span id="placehold"><input class="hold disabled" value="Place hold" type="submit"></span></section>');
  
  
 if (window.location.href.indexOf("/plugin/Koha/Plugin/EDS/opac/eds-search.pl") > -1) {
