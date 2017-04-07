de933ea74a0f84d53c3175fdb1d2731879e76d5f
Michael Cabus <alotofmath@gmail.com>
2017-04-07 08:15:53 -0400

    lists.js
    patch to update lists

---
+++ list.js
@@ -0,0 +1,5 @@
+var addTo = $("#addto").html();
+
+console.log(addTo);
+
+$( "#facetcontainer" ).append('<section class="save_title"><section class="heading"><h4><a class="" href="#">Save Titles</a></h4></section><h4 class="save_text">Save Selected Titles To</h4><select>' + addTo + '</select><section class="save_action"><span class="checkall"><a id="CheckAll" href="#">Select all</a></span><span class="clearall"><a id="CheckNone" href="#">Clear all</a></span></section><input class="btn btn-small" value="Save" type="submit"><span id="placehold"><input class="hold disabled" value="Place hold" type="submit"></span></section>');
